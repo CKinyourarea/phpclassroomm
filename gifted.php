c95d8d5e495a73220f36ddfa8b16fdf1f5828342
CKinyourarea <73819182+CKinyourarea@users.noreply.github.com>
2022-01-14 14:13:23 +0700

    Create gifted.php

---
+++ gifted.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+  <title> G I F T E D </title>
+  <style>
+    #s2{
+      color:purple;
+    }
+    body{
+      background-color: yellow;
+    }
+  </style>
+</head>
+<body>
+  <h1 id="s2"> ยินดีต้อนรับสู่ห้อง GIFTED </h1>
+</body>
+</html>
