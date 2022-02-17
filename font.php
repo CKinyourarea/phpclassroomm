5305d883893b7cc6da42935b5965436cc5468431
CKinyourarea <73819182+CKinyourarea@users.noreply.github.com>
2022-02-17 22:04:53 +0700

    Update font.php

--- font.php
+++ font.php
@@ -4,7 +4,7 @@
     <style>
       @font-face{
         font-family: myFont;
-        src: url(MNNTuaklom.woff);
+        src: url(Uranus.woff);
       }
       @font-face{
         font-family: myFont2;
