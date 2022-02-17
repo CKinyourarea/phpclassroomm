05ab3dc397b00de31859bcb02275aecd791c5e55
CKinyourarea <73819182+CKinyourarea@users.noreply.github.com>
2022-02-17 21:16:05 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,29 @@
+<html>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: myFont;
+        src: url(Weekend.woff);
+      }
+      @font-face{
+        font-family: myFont2;
+        src: url(gigi%20.woff);
+      }
+      #font1{
+        font-family: myFont;
+      }
+      #font2{
+        font-family: myFont2;
+      }
+    </style>
+  </head>
+  <body>
+    <div id="font1">
+      <h1>นางสาวชิษณุชา กระจ่างกิจใจชุ่ม เลขที่ 1 ห้อง 5/13</h1>
+    </div>
+    <div id="font2">
+      <h1>นางสาวชิษณุชา กระจ่างกิจใจชุ่ม เลขที่ 19 ห้อง 5/13</h1>
+    </div>
+  </body>
+</html>
